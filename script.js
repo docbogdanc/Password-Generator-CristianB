b5a639318cce0b1288475bf32397e69cce628f13
docbogdanc <docbogdanc@yahoo.com>
2024-01-09 16:29:13 +0000

    half of the code cleaned

--- script.js
+++ script.js
@@ -90,90 +90,102 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // define array that will contain the characteristic of the password (length, if it has special char,...)
+  var passwordChoices = [];
 
-    // define array that will contain the characteristic of the password (length, if it has special char,...)
-    var passwordChoices = [];
-
-    // creating a condition to restart if condition is not met
-    var condition1 = true;
-    while (condition1 === true) {
-      // asking user to chose the length of the password and convert it to integer
-      var lengthPassword = parseInt(
-        prompt("The password should have between 8 and 128 characters, and at least one of the character types. How many characters should the password have?")
+  // creating a condition to restart if condition is not met
+  var condition1 = true;
+  while (condition1 === true) {
+    // asking user to chose the length of the password and convert it to integer
+    var lengthPassword = parseInt(
+      prompt(
+        "The password should have between 8 and 128 characters, and at least one of the character types. How many characters should the password have?"
+      )
+    );
+    console.log("The password is " + lengthPassword + " characters long");
+    // check if the length of the password is correct ; if yes, add the value to "passwordChoice" array
+    if (lengthPassword >= 8 && lengthPassword <= 128) {
+      passwordChoices.push(lengthPassword);
+      condition1 = false;
+    } else {
+      alert(
+        "Your choice is not respecting the requested criteria. Let's start again."
       );
-      console.log("The password is "+lengthPassword+" characters long");
-      // check if the length of the password is correct , if yes, add the value to "passwordChoice"
-      if (lengthPassword >= 8 && lengthPassword <= 128) {
-        passwordChoices.push(lengthPassword);
-        condition1 = false;
-      } else {
-        alert("Your choice is not respecting the requested criteria. Let's start again.");
-      }
     }
+  }
 
-    //  create another conditional to assertain that at least a password characteristic has been chosen
-    var condition2 = true;
-    while (condition2 === true){
-    //  use input asked for choices regarding password characters
+  //  create another conditional to assertain that at least a password characteristic has been chosen
+  var condition2 = true;
+  while (condition2 === true) {
+    //  user is asked for choices regarding password characters
     var choiceSpecChar = confirm("Do you want the password to have special characters?");
     var choiceNumber = confirm("Do you want to have numbers?");
     var choiceLowerLetters = confirm("Do you want to have lower case letters?");
     var choiceUpperLetters = confirm("Do you want to have upper case letters?");
-    if ((choiceSpecChar===true) || (choiceNumber===true) || (choiceLowerLetters===true) || (choiceUpperLetters===true)){
+    if (choiceSpecChar === true || choiceNumber === true || choiceLowerLetters === true || choiceUpperLetters === true) {
       passwordChoices.push(choiceSpecChar);
       passwordChoices.push(choiceNumber);
       passwordChoices.push(choiceLowerLetters);
       passwordChoices.push(choiceUpperLetters);
       condition2 = false;
-     } else {
-      alert("You have to choose at least one of the password criteria. Let's start again:")
-     }
+    } else {
+      alert("You have to choose at least one of the password criteria. Let's start again:");
     }
-    return passwordChoices
   }
-
-var finalPasswordChoices = getPasswordOptions();
-console.log("the array with user choices: "+finalPasswordChoices); // the result is an array like [length, true, true, true, false]
-
-// Function to create an array of characters accepted for the password
-function createArrayChosenChar() {
-  var allChosenCharacters = []
-  if (finalPasswordChoices[1] ===true) {
-    allChosenCharacters=allChosenCharacters.concat(specialCharacters);
-  } else {console.log("Special characters are omitted from password");}
-  if (finalPasswordChoices[2] ===true) {
-    allChosenCharacters=allChosenCharacters.concat(numericCharacters);
-  } else {console.log("Numeric characters are omitted from password");}
-  if (finalPasswordChoices[3] ===true) {
-    allChosenCharacters=allChosenCharacters.concat(lowerCasedCharacters);
-  } else {console.log("Lower case chhracters are omitted from password");}
-  if (finalPasswordChoices[4] ===true) {
-    allChosenCharacters=allChosenCharacters.concat(upperCasedCharacters);
-  } else {console.log("Upper case characters are omitted from password");}
-  return allChosenCharacters
+  return passwordChoices;
 }
 
+// call the function and store the result ;  the result is an array like [length, true, true, true, false]
+var finalPasswordChoices = getPasswordOptions();
+console.log("the array with user choices: " + finalPasswordChoices);
+
+// Function to create an array of characters corespunding to the array above
+function createArrayChosenChar() {
+  var allChosenCharacters = [];
+  if (finalPasswordChoices[1] === true) {
+    allChosenCharacters = allChosenCharacters.concat(specialCharacters);
+  } else {
+    console.log("Special characters are omitted from password");
+  }
+  if (finalPasswordChoices[2] === true) {
+    allChosenCharacters = allChosenCharacters.concat(numericCharacters);
+  } else {
+    console.log("Numeric characters are omitted from password");
+  }
+  if (finalPasswordChoices[3] === true) {
+    allChosenCharacters = allChosenCharacters.concat(lowerCasedCharacters);
+  } else {
+    console.log("Lower case chhracters are omitted from password");
+  }
+  if (finalPasswordChoices[4] === true) {
+    allChosenCharacters = allChosenCharacters.concat(upperCasedCharacters);
+  } else {
+    console.log("Upper case characters are omitted from password");
+  }
+  return allChosenCharacters;
+}
+
+// call the function and store the result ; the results is an array of separated characters
 var finalArrayWithChar = createArrayChosenChar();
-// finalArrayWithChar = finalArrayWithChar.join('')  //convert array to a single string of characters
+finalArrayWithChar = finalArrayWithChar.join('')  //convert the array to a single string of characters
 console.log(finalArrayWithChar);
-console.log("length of password: "+finalPasswordChoices[0]);
 
 var generatedPassword = [];
 // Function for getting a random element from an array , and creating the password
 function getRandom(arr) {
-  for (i=0 ; i<=finalPasswordChoices[0]; i++) {
+  for (i = 0; i <= finalPasswordChoices[0]; i++) {
     console.log(arr);
-    console.log("array length is "+arr.length);
-    var randomIndex = Math.floor(Math.random()*arr.length);
+    console.log("array length is " + arr.length);
+    var randomIndex = Math.floor(Math.random() * arr.length);
     console.log(randomIndex);
     var passwordElement = arr[randomIndex];
     console.log(passwordElement);
     generatedPassword.push(passwordElement);
     console.log(generatedPassword);
   }
-  return generatedPassword
+  return generatedPassword;
 }
-var passwordFinal = getRandom(finalArrayWithChar)
+var passwordFinal = getRandom(finalArrayWithChar);
 console.log(passwordFinal);
 
 // Function to generate password with user input
@@ -190,7 +202,6 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
